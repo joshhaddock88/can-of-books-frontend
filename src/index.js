fd3b3702f4a1dc44c344348401795ba54ef7fa6b
Joshua Haddock <joshhaddock88@gmail.com>
2021-06-21 14:17:11 -0700

    Create repo files, initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // TODO: wrap everything in Auth0
 ReactDOM.render(
