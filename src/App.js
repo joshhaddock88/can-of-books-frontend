bc0c3b54400067587e45e83ab6386ff562e50eba
Steven_Boston <steven.cole.boston@gmail.com>
2021-06-21 21:28:50 -0700

    completed lab-11

--- src/App.js
+++ src/App.js
@@ -11,11 +11,23 @@ import {
   Switch,
   Route
 } from 'react-router-dom';
+import axios from 'axios';
 
 class App extends React.Component {
+  serverRequest = async () => {
+    const { getIdTokenClaims } = this.props.auth0;
+    let tokenClaims = await getIdTokenClaims();
+    const jwt = tokenClaims.__raw;
+    console.log(jwt);
+    const config = {
+      headers: {'Authorization' : `Bearer ${jwt}`}
+    };
 
+    const requestToken = await axios.get('localhost:3001/test',config);
+
+    console.log(requestToken);
+  };
   render() {
-    console.log('app', this.props);
     const { isAuthenticated } = this.props.auth0;
     return(
       <>
@@ -27,7 +39,7 @@ class App extends React.Component {
                 {isAuthenticated ? <BestBooks/> : <Login/>}
               </Route>
               <Route exact path='/profile'>
-                {isAuthenticated ? <Profile/> : ''}
+                {isAuthenticated ? <Profile request={this.serverRequest}/> : ''}
               </Route>
             </Switch>
             <Footer />
